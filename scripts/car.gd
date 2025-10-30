9bbbcdf69e3a8193ec77b4938aef8d1ebcefe3f4
Sidharth <sidharthsuresh07@gmail.com>
2025-10-30 18:05:17 -0400

    Refactor code structure for improved readability and maintainability; removed redundant code and optimized functions.

--- scripts/car.gd
+++ scripts/car.gd
@@ -1,2 +1,15 @@
 class_name Car
 extends CharacterBody2D
+
+@export var player_controlled: bool = false
+@export var acceleration: float = 10
+@export var max_speed = 300 # player could have the max speed of 400
+
+var mov_direction: Vector2 = Vector2.ZERO
+
+func _process(delta: float) -> void:
+	if player_controlled:
+		pass
+
+func _physics_process(delta: float) -> void:
+	pass
